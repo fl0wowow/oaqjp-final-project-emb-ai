d8ee5903bb6a3bbac674cf588b11867d1fd01f6c
Patryk <patryksarnecki94@gmail.com>
2026-03-13 23:58:17 -0400

    New function emotion_detection.py added

---
+++ emotion_detection.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } } 
+    response = requests.post(url, headers=header, json=input_json)
+    formatted_response = json.loads(response.text)
+    emotions =  formatted_response['emotionPredictions'][0]['emotion']
+    highest_value = max([value for value in emotions.values()])
+    for key, value in emotions.items():
+        if value == highest_value:
+            dominant_emotion = key
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    response = {'anger': anger,
+                'disgust': disgust,
+                'fear': fear,
+                'joy': joy,
+                'sadness': sadness,
+                'dominant_emotion': dominant_emotion 
+                }
+    return response
